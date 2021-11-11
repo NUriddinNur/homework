ccb216421e5ae63c6448f66e02648e661219d0d6
Nuriddin <Nurik900707@gmail.com>
2021-11-11 22:31:07 +0500

    task 1 create user function

---
+++ homework_5.js
@@ -0,0 +1,87 @@
+
+// task 1  +
+
+function createUser(firstName, lastName, status, phone, email, password) {
+    const user = {
+        user_id: createId()+firstName,
+        firstName: checkLeng(firstName, "firstName"),
+        lastName: checkLeng(lastName, "lastName"),
+        status: checkStatus(status),
+        email: checkEmail(email),
+        password: hashingPass(password),
+        phone: checkPhoneKod(phone)
+    }
+    return user;
+}
+
+function createId() {
+    const newId = Date.now();
+    return newId;
+}
+
+function checkStatus(status) {
+    const stat = ["moderator", "user", "admin"]
+    if (stat.includes(status)) {
+        return status;
+    }else {
+        console.log("Statusni qayta kiriting !!! [moderator, user, admin]");
+    }
+}
+
+function checkLeng(inputData, key) {
+    if (typeof inputData != "string") {
+        console.log("String bo'lmagan malumot kiritdingiz !!!");
+        return undefined;
+    }
+    if (inputData.length < 3) {
+        if (key === "firstName"){
+            console.log("Ismingizni qayta kiriting !!!");
+            return undefined;
+        }else if (key === "lastName") {
+            console.log("Last name ni qayta kiriting !!!");
+            return undefined;
+        }
+    }
+    return inputData
+}
+
+function checkEmail(email) {
+    if (email.includes("@")) {
+        return email
+    console.log(phoneKod);
+} else {
+        console.log("Email qayta kiriting");
+    }
+}
+
+function hashingPass(pass) {
+    if (pass.length < 8) {
+        console.log("Password 8 ta belgidan kam");
+        return undefined
+    }
+    let pass1 = [];
+    let pass2 = [];
+    for (let i = 0; i < 4; i ++) {
+        pass1.push(pass[i]);
+    }
+    for (let i = pass.length - 3; i < pass.length; i++) {
+        pass2.push(pass[i])
+    }
+
+    pass = pass1.join("")+"aabbss"+pass2.join("")
+    return pass
+}
+
+function checkPhoneKod(num) {
+    let phoneKod = num.slice(0, 4)
+    if (phoneKod != "+998") {
+        console.log("O'zbekiston telifon raqam kiriting !!! ");
+        return
+    }
+    return num;
+}
+
+const Ali = createUser("Ali", "Valiev", "admin", "+998989091122", "admin@gmail.com", "11223344");
+console.log(Ali);
+
+
