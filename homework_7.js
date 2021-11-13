9c4af26dd69463b36b19cddc47bfc24764103380
Nuriddin <Nurik900707@gmail.com>
2021-11-13 17:55:17 +0500

    clone array

--- homework_7.js
+++ homework_7.js
@@ -6,3 +6,16 @@ const testData = function(data) {
 }
 
 console.log(testData("hello"));    // false
+
+// task 7.2    +
+
+const cloneArray = function(arr1) {
+    const newArr = [...arr1];
+    return newArr
+}
+
+let arr1 = [1,2,3];
+let arr2 = cloneArray(arr1);
+arr2[0] = 5
+
+console.log(arr1, arr2);
